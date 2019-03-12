6ae79ce8816275139cd7b977b11019fece3bad2f
Brendoon Ryos <bryos@MacBook-Pro-de-Brendoon.local>
2019-03-12 18:18:05 -0300

    Adding Resources

---
+++ mobile-challenge/mobile-challenge/Resources/ColorPalette.swift
@@ -0,0 +1,16 @@
+//
+//  ColorPalette.swift
+//  mobile-challenge
+//
+//  Created by Brendoon Ryos on 07/03/19.
+//  Copyright © 2019 Brendoon Ryos. All rights reserved.
+//
+
+import UIKit
+
+enum ColorPalette {
+  static let darkBlue = UIColor(hex: "092283")
+  static let blue = UIColor(hex: "005AB4")
+  static let lightBlue = UIColor(hex: "0097CE")
+  static let white = UIColor(hex: "FFFFFF")
+}
